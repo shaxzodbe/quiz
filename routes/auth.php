061a14bceff198d5d60dc62660c9cf04b30e400c
Shaxzodbek Sulaymanov <sulaymanovshaxzod@gmail.com>
2023-05-11 16:33:37 +0500

    Changed auth.php

--- routes/auth.php
+++ routes/auth.php
@@ -8,10 +8,15 @@ use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\SocialiteController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
+    Route::get('auth/{provider}/redirect', [SocialiteController::class, 'loginSocial'])
+        ->name('socialite.redirect');
+    Route::get('auth/{provider}/callback', [SocialiteController::class, 'callbackSocial'])
+        ->name('socialite.auth');
     Route::get('register', [RegisteredUserController::class, 'create'])
                 ->name('register');
 
