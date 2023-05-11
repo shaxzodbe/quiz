5cba5feb1b357e0546ca203ceb00ffaecb20e3ec
Shaxzodbek Sulaymanov <sulaymanovshaxzod@gmail.com>
2023-05-11 17:42:28 +0500

    QuestionList.php route added to web.php

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::middleware('isAdmin')->group(function () {
+        Route::get('/questions', \App\Http\Livewire\Questions\QuestionList::class)
+            ->name('questions');
+    });
 });
 
 require __DIR__.'/auth.php';
