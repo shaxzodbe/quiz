cfebe5b4dfe3a8487a853e65a523689994f48786
Shaxzodbek Sulaymanov <sulaymanovshaxzod@gmail.com>
2023-05-11 17:40:48 +0500

    QuestionList Livewire component created

---
+++ app/Http/Livewire/Questions/QuestionList.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Questions;
+
+use Livewire\Component;
+
+class QuestionList extends Component
+{
+    public function render()
+    {
+        return view('livewire.questions.question-list');
+    }
+}
