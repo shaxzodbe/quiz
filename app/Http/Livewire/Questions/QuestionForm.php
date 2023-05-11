e350c07662d8d46a84f49900f27eb17903574cac
Shaxzodbek Sulaymanov <sulaymanovshaxzod@gmail.com>
2023-05-11 18:13:46 +0500

    Created QuestionForm.php and question-form.blade.php

---
+++ app/Http/Livewire/Questions/QuestionForm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Questions;
+
+use Livewire\Component;
+
+class QuestionForm extends Component
+{
+    public function render()
+    {
+        return view('livewire.questions.question-form');
+    }
+}
