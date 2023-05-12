2fa17832b613f9c97527ad3484173a8f37bae6f3
Shaxzodbek Sulaymanov <sulaymanovshaxzod@gmail.com>
2023-05-13 01:06:29 +0500

    Created and Show livewire component

---
+++ app/Http/Livewire/Front/Quizzes/Show.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Front\Quizzes;
+
+use Livewire\Component;
+
+class Show extends Component
+{
+    public function render()
+    {
+        return view('livewire.front.quizzes.show');
+    }
+}
