c47fc1cefb802adc165be0daf3ab9151f6d20d75
Shaxzodbek Sulaymanov <sulaymanovshaxzod@gmail.com>
2023-05-12 14:20:48 +0500

    Created QuizForm.php model with quiz-form.blade.php view

---
+++ app/Http/Livewire/Quiz/QuizForm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Quiz;
+
+use Livewire\Component;
+
+class QuizForm extends Component
+{
+    public function render()
+    {
+        return view('livewire.quiz.quiz-form');
+    }
+}
