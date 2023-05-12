dd936c6aedada582f18c6f5a179f45ff55b582a8
Shaxzodbek Sulaymanov <sulaymanovshaxzod@gmail.com>
2023-05-13 00:04:13 +0500

    Changed HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class HomeController extends Controller
 {
-    //
+    public function index(): View
+    {
+        return view('home');
+    }
 }
