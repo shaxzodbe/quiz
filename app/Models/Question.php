da43dcdf4e2a838c632fd0013b94a27c7f63f86c
Shaxzodbek Sulaymanov <sulaymanovshaxzod@gmail.com>
2023-05-12 17:47:07 +0500

    Added Relation BelongsToMany between Question and Quiz

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -17,9 +18,12 @@ class Question extends Model
         'answer_explanation',
         'more_info_link'
     ];
-
     public function questionOptions(): HasMany
     {
         return $this->hasMany(QuestionOption::class)->inRandomOrder();
+    }
+    public function quiz(): BelongsToMany
+    {
+        return $this->belongsToMany(Quiz::class);
     }
 }
