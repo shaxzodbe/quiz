18b609a537f534a533fd541c39d15e4e3777f0b8
Shaxzodbek Sulaymanov <sulaymanovshaxzod@gmail.com>
2023-05-12 17:42:13 +0500

    Changed migration question_quiz table

--- database/migrations/2023_05_12_123808_create_question_quiz_table.php
+++ database/migrations/2023_05_12_123808_create_question_quiz_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('question_quiz', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->foreignId('question_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('quiz_id')->constrained()->cascadeOnDelete();
         });
     }
 
