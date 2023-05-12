2c5b072a3978f604666aaac7f0091e38a0f0b528
Shaxzodbek Sulaymanov <sulaymanovshaxzod@gmail.com>
2023-05-12 10:55:43 +0500

    Changed Quiz.php model with migration

--- app/Models/Quiz.php
+++ app/Models/Quiz.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Quiz extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+    protected $fillable = [
+        'title',
+        'slug',
+        'description',
+        'published',
+        'public'
+    ];
+    protected $casts = [
+        'published' => 'boolean',
+        'public' => 'boolean',
+    ];
 }
--- database/migrations/2023_05_12_054816_create_quizzes_table.php
+++ database/migrations/2023_05_12_054816_create_quizzes_table.php
@@ -13,7 +13,13 @@ return new class extends Migration
     {
         Schema::create('quizzes', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug')->nullable();
+            $table->longText('description')->nullable();
+            $table->boolean('published')->default(0)->nullable();
+            $table->boolean('public')->default(0)->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
