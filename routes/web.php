a2bc580f7e2311df8c0fa7a5c53a26e3932dbd67
Shaxzodbek Sulaymanov <sulaymanovshaxzod@gmail.com>
2023-05-13 00:46:49 +0500

    Views changed

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::middleware('auth')->group(function () {
     Route::get('profile', [ProfileController::class, 'edit'])->name('profile.edit');
