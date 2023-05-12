781f13ef0b84e47fb49ac265d1505df211e65759
Shaxzodbek Sulaymanov <sulaymanovshaxzod@gmail.com>
2023-05-12 15:27:10 +0500

    Changed QuizForm.php and quiz-form.blade.php

--- app/Http/Livewire/Quiz/QuizForm.php
+++ app/Http/Livewire/Quiz/QuizForm.php
@@ -2,12 +2,65 @@
 
 namespace App\Http\Livewire\Quiz;
 
+use App\Models\Quiz;
+use Illuminate\Routing\Redirector;
+use Illuminate\Support\Str;
 use Livewire\Component;
 
 class QuizForm extends Component
 {
+    public Quiz $quiz;
+    public bool $editing = false;
+
+    public function mount(Quiz $quiz): void
+    {
+        $this->quiz = $quiz;
+        if ($this->quiz->exists) {
+            $this->editing = true;
+        } else {
+            $this->quiz->published = false;
+            $this->quiz->public = false;
+        }
+    }
+
+    public function updatedQuizTitle(): void
+    {
+        $this->quiz->slug = Str::slug($this->quiz->title);
+    }
+
+    public function save(): Redirector
+    {
+        $this->validate();
+        $this->quiz->save();
+        return to_route('quizzes');
+    }
+
     public function render()
     {
         return view('livewire.quiz.quiz-form');
+    }
+
+    public function rules(): array
+    {
+        return [
+            'quiz.title' => [
+                'string',
+                'required'
+            ],
+            'quiz.slug' => [
+                'string',
+                'nullable'
+            ],
+            'quiz.description' => [
+                'string',
+                'nullable'
+            ],
+            'quiz.published' => [
+                'boolean',
+            ],
+            'quiz.public' => [
+                'boolean',
+            ],
+        ];
     }
 }
