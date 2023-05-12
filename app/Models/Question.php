1eb26780ebc3ad419878138f730965e31e7522cd
Shaxzodbek Sulaymanov <sulaymanovshaxzod@gmail.com>
2023-05-12 09:54:51 +0500

    Added relation to QuestionOption.php in Question.php

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Question extends Model
@@ -16,4 +17,9 @@ class Question extends Model
         'answer_explanation',
         'more_info_link'
     ];
+
+    public function questionOptions(): HasMany
+    {
+        return $this->hasMany(QuestionOption::class);
+    }
 }
